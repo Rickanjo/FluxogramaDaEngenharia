161050e7b8407665ad6895aaacc13d2228a0a41a
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-26 21:26:42 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Faça um programa que implemente o meme Fluxograma da Engenharia.
+Console.WriteLine("--- Fluxograma Da Engenharia ---");
+
+Console.Write("O objetivo está se movendo (S/N)? ");
+string movendo = Console.ReadLine()!;
+
+if (movendo == "S") {
+    Console.Write("Deveria? (S/N) ");
+    String deveria = Console.ReadLine()!;
+
+    if (deveria == "S") {
+        Console.WriteLine("ÓTIMO ");
+    } else if (deveria == "N") {
+        Console.WriteLine("Use Silver Tape. ");
+    } else {
+        Console.WriteLine("Opção Inválida!");
+    }
+}
+else if (movendo == "N") {
+    Console.Write("Deveria? (S/N) ");
+    string deveria = Console.ReadLine()!;
+
+    if (deveria == "S") {
+        Console.WriteLine("Use WD-40. ");
+    } else if (deveria == "N") {
+        Console.WriteLine("Ótimo! ");
+    }else {
+        Console.WriteLine("opção inválida! ");
+    }
+} else {
+    Console.WriteLine("Opção inválida! ");
+}
+
+
+
+
